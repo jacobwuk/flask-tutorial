079b8e2ec6e042ed418f5708ec71dbe8f2babd47
Jacob Wukovits <jacobwuk@buffalo.edu>
2019-10-02 19:29:34 -0400

    Link database commands to instances created by this file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -17,7 +17,7 @@ def create_app(test_config=None):
         # load the provided config
         app.config.from_mapping(test_config)
 
-    # ensure the app.instance_path exists, as Flask does not create this
+    # ensure the app.instance_path exists (as Flask does not create this)
     # The SQLite database will get put there
     try:
         os.makedirs(app.instance_path)
@@ -28,5 +28,12 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World'
+
+    # complete the link of init_db_command and close_db with the
+    # app instance that is being created
+    # The instance is created by the factory, and isn't directly available
+    # when writing the functions
+    from . import db
+    db.init_app(app)
 
     return app
